03c3c5a25ac30f214158f3aa06616843aab1eb00
wvcneto <wvcneto@gmail.com>
2020-10-27 10:30:23 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import RichTextEditor from './components/RichTextEditor';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RichTextEditor />
+    </div>
+  );
+}
+
+export default App;
